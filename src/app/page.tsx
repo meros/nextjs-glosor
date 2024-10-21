187115b30d1f5efcecbe5d7aa1aa3e57c39c82be
Alexander Schrab <meros@Airsomtlexander.localdomain>
2024-10-21 19:55:05 +0200

    Remove placeholder main page, redirect instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <ul>
-        <li>
-          <Link href="/edit">Go to Edit Page</Link>
-        </li>
-        <li>
-          <Link href="/train">Go to Train Page</Link>
-        </li>
-      </ul>
-    </div>
-  );
+  redirect('/edit');
+
+  return null;
 }
